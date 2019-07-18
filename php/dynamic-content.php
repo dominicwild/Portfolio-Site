eea5bf4a1e2a21e5a17ef755a3a5fc0fcab3e9d8
dominicwild <dominic55555@hotmail.com>
2019-07-18 05:14:10 +0100

    Added preview hover overlay and more project content.

--- php/dynamic-content.php
+++ php/dynamic-content.php
@@ -54,9 +54,19 @@ $languages = [
         "title" => "Python",
     ],
     "c++" => [
-        "icon" => "https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/cplusplus.svg",
+        "icon" => "/portfolioImages/icons/cplusplus/cplusplus-plain.svg",
         "description" => "Micro:bit Mesh network protocol and minor projects",
         "title" => "C++",
+    ],
+    "c" => [
+        "icon" => "/portfolioImages/icons/c/c-plain.svg",
+        "description" => "",
+        "title" => "C",
+    ],
+    "c#" => [
+        "icon" => "/portfolioImages/icons/csharp/csharp-plain.svg",
+        "description" => "Unity Augmented Reality App using Vuforia",
+        "title" => "C#",
     ],
     "R" => [
         "icon" => "https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/r.svg",
@@ -67,6 +77,16 @@ $languages = [
         "icon" => "portfolioImages/matlab.svg",
         "description" => "Used for statistical analysis, plots, media coding and processing",
         "title" => "R",
+    ],
+    "mysql" => [
+        "icon" => "https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/mysql.svg",
+        "description" => "",
+        "title" => "MySQL",
+    ],
+    "aws" => [
+        "icon" => "/portfolioImages/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg",
+        "description" => "",
+        "title" => "Amazon Web Services",
     ],
 ];
 
@@ -100,54 +120,96 @@ $projects = [
     "Portfolio Site" => [
         "name" => "Portfolio Site",
         "description" => "
-        I made this site completely from scratch, only using Bootstrap. I took this opportunity to make the site in a mobile first manner, making it completely responsive was a goal of this project. 
-        <p> 
-        With this project I also put to use some lessons I learned in my prior social network project. Some key aspects I explored in this project were: Use of PHP for templating repetitive page elements, using Google fonts, using CSS animations, responsive design and content management. 
-        </p>
-        <p>
-        One design decision I made on this site was not to use a database. As this seemed overkill for a static website (no complex data or data relationships), at least at the scale it is now. Being much easier to manage with PHP arrays that dynamically create content, essentially a database as PHP arrays instead.
-        </p>
-        <p>
-        This website will continue to grow over time as more projects get developed. At the moment the layout is fairly simple and I plan to evolve it into something more visually appealing in the future as a passion project. Some usability improvements are also planned.
-        </p>
+            I made this site completely from scratch, only using Bootstrap. I took this opportunity to make the site in a mobile first manner, making it completely responsive was a goal of this project. 
+            <p> 
+            With this project I also put to use some lessons I learned in my prior social network project. Some key aspects I explored in this project were: Use of PHP for templating repetitive page elements, using Google fonts, using CSS animations, responsive design and content management. 
+            </p>
+            <p>
+            One design decision I made on this site was not to use a database. As this seemed overkill for a static website (no complex data or data relationships), at least at the scale it is now. Being much easier to manage with PHP arrays that dynamically create content, essentially a database as PHP arrays instead.
+            </p>
+            <p>
+            This website will continue to grow over time as more projects get developed. At the moment the layout is fairly simple and I plan to evolve it into something more visually appealing in the future as a passion project. Some usability improvements are also planned.
+            </p>
         ",
         "start_date" => strtotime("July 2019"),
         "image" => "portfolioImages/portfolio-site.png",
         "languages" => [
             $languages["php"], $languages["bootstrap"], $languages["css3"], $languages["html5"]
         ],
+        "github" => "https://github.com/dominicwild/Portfolio-Site",
     ],
     "Social Network" => [
-        "name" => "Social Network",
-        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque enim vel mauris tempor, sed tincidunt nunc ullamcorper. Aliquam suscipit ante laoreet, viverra lorem ac, finibus nibh. Phasellus sed turpis a enim vestibulum varius. Sed a velit ut est molestie vehicula. Vestibulum imperdiet pellentesque enim at iaculis. Fusce purus erat, congue vitae egestas vel, accumsan non ex. Donec maximus elit eget turpis viverra, et dictum erat lobortis. Duis consequat suscipit eros, in sagittis nulla dictum non. Integer sed est sed ligula pellentesque varius quis non massa. Ut mollis aliquet eleifend. Suspendisse varius, metus a porttitor gravida, nulla eros fringilla ex, eu facilisis tellus urna eu odio. Maecenas vel tortor nec neque sodales hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. In pharetra, nibh vel consectetur sagittis, ipsum velit tempus sem, a blandit quam metus ac nibh. Sed id nunc massa.
-
-                            Praesent in placerat purus. Maecenas fermentum lobortis ante vel sodales. Sed dolor sapien, scelerisque eget augue at, sollicitudin condimentum odio. Donec turpis felis, sagittis nec nisl sit amet, facilisis elementum nibh. Phasellus viverra, justo et auctor euismod, nulla dolor congue erat, at maximus tortor est sit amet turpis. Nulla nunc sem, tempor ac risus ut, vehicula sodales nisi. Mauris feugiat, sapien ac laoreet accumsan, eros urna gravida tellus, et posuere justo lorem id neque.
-
-                            Suspendisse potenti. Nullam nec cursus metus, sed gravida est. Integer mattis quam quis velit vehicula, in dignissim odio feugiat. Vivamus quis nulla sagittis justo dictum rutrum ac eget urna. Phasellus ut lectus id massa laoreet fringilla. Pellentesque lobortis enim non aliquet lacinia. Morbi mattis felis sit amet bibendum imperdiet. Quisque dictum, dolor ultricies lobortis eleifend, velit mi venenatis urna, quis facilisis sem justo ut sapien. Sed eget elementum leo. Fusce sit amet augue elit. Nulla eget ullamcorper nisi.",
-        "start_date" => strtotime("April 2019"),
-        "image" => "portfolioImages/gifs/social.gif",
-        "languages" => [
-            $languages["cakephp"], $languages["php"], $languages["bootstrap"], $languages["css3"], $languages["html5"]
-        ],
-        "github" => "www.google.com",
-        "liveDeployment" => "wwww.dominicwild.com/social/",
-    ],
-    "Social Network3" => [
-        "name" => "Social Network",
-        "description" => "Suspendisse potenti. Nullam nec cursus metus, sed gravida est. Integer mattis quam quis velit vehicula, in dignissim odio feugiat. Vivamus quis nulla sagittis justo dictum rutrum ac eget urna. Phasellus ut lectus id massa laoreet fringilla. Pellentesque lobortis enim non aliquet lacinia. Morbi mattis felis sit amet bibendum imperdiet. Quisque dictum, dolor ultricies lobortis eleifend, velit mi venenatis urna, quis facilisis sem justo ut sapien. Sed eget elementum leo. Fusce sit amet augue elit. Nulla eget ullamcorper nisi.",
+        "name" => "Enterprise Social Network (ESN)",
+        "description" => "
+            This project was undertaken as part of my university placement. It involved developing a social networking site through the full software development life cycle (Analyis, design, implementation, testing and evaluation). The company was facing some internal social communication problems, no knew what events were happening or what their co-workers were up to. Initial need-finding took place via interviews and a company wide questionnaire, data quantized, analyzed and requirements were derived, among a Minimum Viable Product (MVP) plan.
+            <p>
+            The requirements were designed and then implemented successfully, meeting the MVP. The project later went on to form part of my dissertation designing an incremental development technique for Enterprise Social Networks. The ESN was made with CakePHP, which sped up development and taught me a lot about web development, the general architecture and approaches most common frameworks employ to streamline development.
+            </p>
+            <p>
+            During the testing phase, the company was unable to deploy my solution on their services in good time. Therefore over a weekend I decided to move my solution to Amazon AWS. This was my first time deploying a web server on a linux distribution, it involved lots of rapid learning and was a very fun experience. The knowledge I gained from this experience was invaluable, as I'm utilizing it right now, to write and host this portfolio website!
+            </p>
+            <p>
+            The finished product was feature complete containing the following features:
+            <ul>
+                <li>OAuth2 Google authentication</li>
+                <li>User Posts and Comments</li>
+                <li>Image Attachments for Posts</li>
+                <li>User Created Polls</li>
+                <li>Recent Activity Panel</li>
+                <li>Event System (for organising social events)</li>
+                <li>Group System (for categorizing events and posts)</li>
+                <li>Google Calendar Integration (for events)</li>
+                <li>Administration Page</li>
+                <li>HTML Email Notifications for various alerts (new events, comments, posts etc)</li>
+                <li>User Profiles</li>
+                <li>Search Function</li>
+            </ul>
+            </p>
+        ",
         "start_date" => strtotime("January 2019"),
         "image" => "portfolioImages/gifs/social.gif",
         "languages" => [
-            $languages["cakephp"], $languages["php"], $languages["bootstrap"], $languages["css3"], $languages["html5"]
+            $languages["php"], $languages["cakephp"], $languages["mysql"], $languages["bootstrap"], $languages["css3"], $languages["html5"]
+        ],
+        "github" => "https://github.com/dominicwild/SocialNetwork",
+        "liveDeployment" => "social",
+    ],
+    "Snooker Game" => [
+        "name" => "Snooker Pool Game",
+        "description" => "A basic project that was developed for fun entirely through Java. The Snooker game was used as a way to practice object orientated design and tackle the challenges associated with building a game (such as dealing with frames and game logic).
+        
+        <p>
+        The conclusion of this project is a fully implemented game of Snooker, tested for following all the rules correctly.
+        </p>
+        
+        ",
+        "github" => "https://github.com/dominicwild/Java-Pool-Game",
+        "start_date" => strtotime("April 2016"),
+        "image" => "portfolioImages/gifs/snooker.gif",
+        "languages" => [
+            $languages["java"],
         ],
     ],
-    "Social Network4" => [
-        "name" => "Social Network",
-        "description" => "Lorem iset turpis viverra, efeugiat. Vivamus quis nulla sagittis justo dictum rutrum ac eget urna. Phasellus ut lectus id massa laoreet fringilla. Pellentesque lobortis enim non aliquet lacinia. Morbi mattis felis sit amet bibendum imperdiet. Quisque dictum, dolor ultricies lobortis eleifend, velit mi venenatis urna, quis facilisis sem justo ut sapien. Sed eget elementum leo. Fusce sit amet augue elit. Nulla eget ullamcorper nisi.",
-        "start_date" => strtotime("January 2019"),
-        "image" => "portfolioImages/gifs/social.gif",
+    "Micro:bit Mesh" => [
+        "name" => "Micro:bit Mesh",
+        "description" => "Micro:bit Mesh was a mesh network protocol I developed for the BBC Micro:bit. It was a feature added with the aim to teach kids about the computing concept of networking, giving them a tool to play around with.
+        
+        <p>
+        The project involved very low level C/C++ programming using the Micro:bits run time called codal. This project stressed reading and understanding how, at a low level, embedded systems hardware could be manipulated to achieved the desired effects, working a lot with the Micro:bits radio. One of the biggest challenges I faced while doing this project was debugging the Micro:bits, as its difficult to know what programming logic/state each system was in. To aide in this, I made use of the Micro:bits LED's and serial I/O to print debug data.
+        </p>
+        <p>
+        Feasibility tests, design, implementation and evaluation of the protocol was conducted. The protocol was based off Glossy, a controlled packet flooding approach to mesh networks that came up in research. 
+        </p>
+        <p>
+        The conclusion of this project was that this approach is possible, however encountered strange patterns of high packet loss in correlation to distance/physical network topology and therefore requires more thorough investigation from a physics perspective.
+        </p>
+       
+        ",
+        "start_date" => strtotime("October 2017"),
+        "image" => "portfolioImages/microbit.jpg",
+        "github" => "https://github.com/dominicwild/MicroBitMesh",
         "languages" => [
-            $languages["cakephp"], $languages["php"], $languages["bootstrap"], $languages["css3"], $languages["html5"]
+            $languages["c++"], $languages["c"],
         ],
     ],
     "Social Network5" => [
--- php/templates/project.php
+++ php/templates/project.php
@@ -6,6 +6,14 @@
  *
  * @var array $project
  */
+
+
+if((isset($project["github"]) + isset($project["liveDeployment"]) == 1)){
+    $button_cols = "col-12";
+} else {
+    $button_cols = "col-12 col-md-6";
+}
+
 ?>
 
 
@@ -15,7 +23,9 @@
             <img class="w-100 thumbnail" src="<?= $project["image"] ?>">
             <div class="language-set">
                 <?php foreach ($project["languages"] as $language): ?>
+                <span title="<?= $language["title"] . " Icon" ?>">
                     <img class="svg language language-padding" src="<?= $language["icon"] ?>">
+                </span>
                 <?php endforeach; ?>
             </div>
         </div>
@@ -27,26 +37,30 @@
                 <?= $project["description"] ?>
             </p>
 
-            <div class="row buttons">
+            <div class="row buttons d-flex justify-content-center">
                 <?php if (isset($project["github"])): ?>
-                    <div class="col-12 col-md-6 mt-1 px-1">
-                        <button class="btn btn-primary h-100 w-100 d-flex justify-content-center align-items-center github">
-                            View on Github
-                            <img class="svg white-svg"
-                                 src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg">
-                        </button>
+                    <div class="<?= $button_cols ?> mt-1 px-1">
+                        <a href="<?= $project["github"] ?>" target="_blank">
+                            <button class="btn btn-primary h-100 w-100 d-flex justify-content-center align-items-center github">
+                                View on Github
+                                <img class="svg white-svg"
+                                     src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg">
+                            </button>
+                        </a>
                     </div>
 
                 <?php endif; ?>
 
                 <?php if (isset($project["liveDeployment"])): ?>
-                    <div class="col-12 col-md-6 mt-1 px-1">
-                        <button class="btn btn-primary h-100 w-100 d-flex justify-content-center align-items-center live ">
-                            <div class="h-100" style="display:inline-block">
-                                Live Deployment
-                            </div>
-                            <img class="svg white-svg" src="/portfolioImages/live.svg">
-                        </button>
+                    <div class="<?= $button_cols ?> mt-1 px-1">
+                        <a href="<?= $project["liveDeployment"] ?>" target="_blank">
+                            <button class="btn btn-primary h-100 w-100 d-flex justify-content-center align-items-center live ">
+                                <div class="h-100" style="display:inline-block">
+                                    Live Deployment
+                                </div>
+                                <img class="svg white-svg" src="/portfolioImages/live.svg">
+                            </button>
+                        </a>
                     </div>
                 <?php endif; ?>
             </div>
